2305dc16ec3a9cb630ed55ac3cf5c1e789899824
manuk aroyan <9208042@mail.ru>
2018-11-10 15:32:58 +0100

    Initial Commit

---
+++ myOwnApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myOwnApp
+//
+//  Created by manuk aroyan on 10.11.2018.
+//  Copyright © 2018 manuk aroyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
